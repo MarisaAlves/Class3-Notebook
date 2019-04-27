e54bea2a808d260cb1b8f3b847d7a58bf958828f
Marisa Alves <m_lves@cce-fb105-03m.concordia.ca>
2019-04-27 12:06:05 -0400

    changing code to only open if file exists

--- my_csv_reader.py
+++ my_csv_reader.py
@@ -4,9 +4,9 @@ import os
 #if can read file:
 if os.path.isfile('/Users/m_lves/Downloads/toys-datasets/diabetes.data'):
 	print('I have a file to process')
+	file=open('/Users/m_lves/Downloads/toys-datasets/diabetes.data')
+	for line in file.readlines():
+        	print(line)
 else:
 	print('Boo, no file for me.')
 
-file=open('/Users/m_lves/Downloads/toys-datasets/diabetes.data')
-for line in file.readlines():
-	print(line)
